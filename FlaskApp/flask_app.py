96e88675344ac1fd7c2bdc9c0df8a9cda3058b3e
arsingam <sarun.atp@gmail.com>
2021-12-14 15:23:24 +0530

    final

--- FlaskApp/flask_app.py
+++ FlaskApp/flask_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
@@ -11,7 +11,7 @@ def login():
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return "Welcome Home"
 
